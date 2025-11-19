7f086c768e0b62b283c20b431ad5c77bf6658def
ICT-fushibo <fushibo123123123@163.com>
2025-11-19 13:38:23 +0800

    update getdata shell

---
+++ data/getdata.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/datasets/stanford-cs336/owt-sample/resolve/main/owt_train.txt.gz
+gunzip owt_train.txt.gz
+wget https://huggingface.co/datasets/stanford-cs336/owt-sample/resolve/main/owt_valid.txt.gz
+gunzip owt_valid.txt.gz

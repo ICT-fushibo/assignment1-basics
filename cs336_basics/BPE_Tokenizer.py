535c28179e7459008af48960c72e04e024dc1f92
ICT-fushibo <fushibo123123123@163.com>
2025-11-21 22:19:14 +0800

    实现 BPE 编码和解码功能，更新 Tokenizer 类以支持特殊标记处理

--- cs336_basics/BPE_Tokenizer.py
+++ cs336_basics/BPE_Tokenizer.py
@@ -1,6 +1,7 @@
 from typing import Iterable,Iterator
 import os
 import json
+import regex as re
 
 class Tokenizer:
     def __init__(self,vocab:dict[int,bytes],merges:list[tuple[bytes,bytes]],special_tokens:list[str]|None=None):
@@ -13,8 +14,12 @@ class Tokenizer:
         """
         
         self.vocab=vocab
+        self.bytes2int={v:k for k,v in vocab.items()}
         self.merges=merges
         self.special_tokens=special_tokens
+        # sorted_special_tokens=sorted(special_tokens,key=len,reverse=True)
+        self.special_tokens_re="|".join(re.escape(t) for t in sorted(special_tokens,key=len,reverse=True)) if special_tokens else None
+        self.PAT=r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
         
         
     @classmethod
@@ -108,9 +113,70 @@ class Tokenizer:
         Returns:
             list[int]: encode text
         """
-        pass
+        # handel special token
+        if self.special_tokens:
+            chunks=re.split(f"({self.special_tokens_re})",text)
+        else:
+            chunks=[text]
+        
+        # pre-tokenize
+        pre_token_list=[]
+        
+        for chunk in chunks:
+            if self.special_tokens and chunk in self.special_tokens:
+                pre_token_list.append(chunk.encode("utf-8"))
+                continue
+            for m in re.finditer(self.PAT,chunk):
+                token=m.group(0)
+                b_token=token.encode("utf-8")
+                token_list=tuple(bytes([x])for x in b_token)
+                
+                    
+                pre_token_list.append(token_list)
+        
+        # apply merges
+        for merge in self.merges:
+            A,B=merge
+            AB=A+B
+            i=0
+            new_pretoken_list=[]
+            
+            for pre_token in pre_token_list:
+                if A not in pre_token or B not in pre_token or (isinstance(pre_token,bytes) and self.special_tokens and pre_token.decode() in self.special_tokens):
+                    new_pretoken_list.append(pre_token)
+                    continue
+                lst = list(pre_token)
+                i=0
+                new_token_list=[]
+                while i <len(lst):
+                    if i+1<len(lst) and lst[i]==A and lst[i+1]==B:
+                        new_token_list.append(AB)
+                        i+=2
+                    else:
+                        new_token_list.append(lst[i])
+                        i+=1
+                new_token_tup=tuple(new_token_list)
+                new_pretoken_list.append(new_token_tup)
+            
+            pre_token_list=new_pretoken_list
+        
+        # byte 2 int
+        encode_int=[]
+        for pre_token in pre_token_list:
+            if isinstance(pre_token,bytes) and self.special_tokens and pre_token.decode() in self.special_tokens:
+                encode_int.append(self.bytes2int[pre_token])
+                continue
+            for token_bytes in pre_token:
+                encode_int.append(self.bytes2int[token_bytes])
+        
+        return encode_int        
+                
+                    
+                
+        
+        
     
-    def encode_iterable(self,iterable:Iterable[str]) -> Iterator[int]:
+    def encode_iterable(self,iterable:Iterable[str] ) -> Iterator[int]:
         """Given an iterable of strings (e.g., a Python file handle), return a generator that lazily yields token IDs.
 
         Args:
@@ -119,7 +185,12 @@ class Tokenizer:
         Yields:
             Iterator[int]: _description_
         """
-        pass
+        
+        for text_chunk in iterable:
+            token_ids= self.encode(text_chunk)
+            
+            yield from token_ids
+        
     
     def decode(self,ids:list[int]) -> str:
         """Decode a sequence of token IDs into text.
@@ -130,4 +201,66 @@ class Tokenizer:
         Returns:
             str: decode text
         """
-        pass
+        byets_str=b""
+        for id in ids:
+            byets_str+=self.vocab[id]
+        return byets_str.decode("utf-8",errors="replace")
+    
+if __name__=='__main__':
+    
+    FIXTURES_PATH="/home/fu/assignment1-basics/tests/fixtures/"
+    
+    VOCAB_PATH = FIXTURES_PATH + "gpt2_vocab.json"
+    MERGES_PATH = FIXTURES_PATH + "gpt2_merges.txt"
+    
+    def get_tokenizer_from_vocab_merges_path(
+        vocab_path: str | os.PathLike,
+        merges_path: str | os.PathLike,
+        special_tokens: list[str] | None = None,
+    ):
+        from tests.common import gpt2_bytes_to_unicode
+        gpt2_byte_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
+        with open(vocab_path) as vocab_f:
+            gpt2_vocab = json.load(vocab_f)
+        gpt2_bpe_merges = []
+        with open(merges_path) as f:
+            for line in f:
+                cleaned_line = line.rstrip()
+                if cleaned_line and len(cleaned_line.split(" ")) == 2:
+                    gpt2_bpe_merges.append(tuple(cleaned_line.split(" ")))
+        # The GPT-2 tokenizer uses a remapped unicode encoding for bytes. Let's
+        # just return the original bytes, so we don't force students to use
+        # any particular encoding scheme.
+        vocab = {
+            gpt2_vocab_index: bytes([gpt2_byte_decoder[token] for token in gpt2_vocab_item])
+            for gpt2_vocab_item, gpt2_vocab_index in gpt2_vocab.items()
+        }
+        # If any of the special tokens don't exist in the vocab, append them to the vocab.
+        if special_tokens:
+            for special_token in special_tokens:
+                byte_encoded_special_token = special_token.encode("utf-8")
+                if byte_encoded_special_token not in set(vocab.values()):
+                    vocab[len(vocab)] = byte_encoded_special_token
+
+        merges = [
+            (
+                bytes([gpt2_byte_decoder[token] for token in merge_token_1]),
+                bytes([gpt2_byte_decoder[token] for token in merge_token_2]),
+            )
+            for merge_token_1, merge_token_2 in gpt2_bpe_merges
+        ]
+        return Tokenizer(vocab, merges, special_tokens)
+    tokenizer = get_tokenizer_from_vocab_merges_path(
+        vocab_path=VOCAB_PATH,
+        merges_path=MERGES_PATH,
+        special_tokens=["<|endoftext|>","<|endoftext|><|endoftext|>"]
+    )
+    test_string = "Héllò hôw <|endoftext|><|endoftext|> are ü? 🙃<|endoftext|>"
+    encoded_ids = tokenizer.encode(test_string)
+    print(encoded_ids)
+    tokenized_string = [tokenizer.decode([x]) for x in encoded_ids]
+    print(tokenized_string)
+    decoded_string = tokenizer.decode(encoded_ids)
+    print(decoded_string)
+    assert test_string == decoded_string
+    
